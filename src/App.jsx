48c0bbc25b304c1c9f7c5b1c71add0ff0f308d78
ikeshav26 <keshavgilhotra4@gmail.com>
2025-06-26 09:54:00 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='font-sans text-2xl text-center mt-10  '>
+      password manager
+    </div>
+  )
+}
+
+export default App
