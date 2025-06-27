795c26676cbd158ada8cfa34e24644036c9e00d9
ikeshav26 <keshavgilhotra4@gmail.com>
2025-06-27 10:20:56 +0530

    Signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Addpassword from './pages/Addpassword'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
 
 const App = () => {
   return (
-    <div className='font-sans text-2xl text-center mt-10  '>
-      password manager
+    <div className=''>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/addpassword' element={<Addpassword/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
     </div>
   )
 }
---
+++ src/pages/Addpassword.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Addpassword = () => {
+  return (
+    <div>
+      add password page
+    </div>
+  )
+}
+
+export default Addpassword
